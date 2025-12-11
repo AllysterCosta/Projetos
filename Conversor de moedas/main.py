cd0d369c3db92c9268857b3015078e4cbe9b3385
Allyster <allystercosta@gmail.com>
2025-12-11 13:26:43 -0300

    Criando arquivos xml para leituras e extraindo funções para outro arquivo py

--- Conversor de moedas/main.py
+++ Conversor de moedas/main.py
@@ -6,6 +6,7 @@
 # Lista de exibição com uma lista de moedas
 
 import customtkinter as ctk
+from pegar_moedas import nomes_moedas, obter_conversoes
 
 # Criar e configurar a janela principal
 ctk.set_appearance_mode("dark")
@@ -14,13 +15,15 @@ ctk.set_default_color_theme("dark-blue")
 janela = ctk.CTk()
 janela.geometry("500x500")
 
+dict_conversoes_disponiveis = obter_conversoes()
+
 # Criar os botões, textos e outros elementos da interface
-titulo = ctk.CTkLabel(janela, text="Conversor de Moedas")
+titulo = ctk.CTkLabel(janela, text="Conversor de Moedas", font=("", 20))
 texto_moeda_origem = ctk.CTkLabel(janela, text="Selecione moeda de Origem")
 texto_moeda_destino = ctk.CTkLabel(janela, text="Selecione moeda de Destino")
 
 campo_moeda_origem = ctk.CTkOptionMenu(
-    janela, values=["USD", "EUR", "BRL", "JPY", "GBP", "BTC"])
+    janela, values=list(dict_conversoes_disponiveis.keys()))
 campo_moeda_destino = ctk.CTkOptionMenu(
     janela, values=["USD", "EUR", "BRL", "JPY", "GBP", "BTC"])
 
@@ -34,22 +37,19 @@ botao_converter = ctk.CTkButton(
 
 lista_de_moedas = ctk.CTkScrollableFrame(janela)
 
-moedas_disponiveis = ["USD - Dólar Americano",
-                      "EUR - Euro",
-                      "BRL - Real Brasileiro",
-                      "JPY - Iene Japonês",
-                      "GBP - Libra Esterlina",
-                      "BTC - Bitcoin"]
-for moeda in moedas_disponiveis:
-    texto_moeda = ctk.CTkLabel(lista_de_moedas, text=moeda)
+moedas_disponiveis = nomes_moedas()
+for codigo_moeda in moedas_disponiveis:
+    nome_moeda = moedas_disponiveis[codigo_moeda]
+    texto_moeda = ctk.CTkLabel(
+        lista_de_moedas, text=f"{codigo_moeda}: {nome_moeda}")
     texto_moeda.pack()
 
 # Criar todos os elementos da interface gráfica
 titulo.pack(padx=10, pady=10)
 texto_moeda_origem.pack(padx=10, pady=10)
-campo_moeda_origem.pack(padx=10, pady=10)
-texto_moeda_destino.pack(padx=10, pady=10)
-campo_moeda_destino.pack(padx=10, pady=10)
+campo_moeda_origem.pack(padx=10)
+texto_moeda_destino.pack(padx=10, pady=3)
+campo_moeda_destino.pack(padx=10)
 botao_converter.pack(padx=10, pady=10)
 lista_de_moedas.pack(padx=10, pady=10, fill="both", expand=True)
 
---
+++ Conversor de moedas/pegar_moedas.py
@@ -0,0 +1,28 @@
+import xmltodict
+
+
+def nomes_moedas():
+
+    with open("moedas.xml", "rb") as arquivo_moedas:
+        dic_moedas = xmltodict.parse(arquivo_moedas)
+
+    moedas = dic_moedas["xml"]
+    print(moedas)
+    return moedas
+
+
+def obter_conversoes():
+    with open("conversoes.xml", "rb") as arquivo_conversoes:
+        dic_conversoes = xmltodict.parse(arquivo_conversoes)
+
+    conversoes = dic_conversoes["xml"]
+    dict_conversoes_disponiveis = {}
+    for par_conversao in conversoes:
+        moeda_origem, moeda_destino = par_conversao.split("-")
+        if moeda_origem in dict_conversoes_disponiveis:
+            dict_conversoes_disponiveis[moeda_origem].append(moeda_destino)
+        else:
+            dict_conversoes_disponiveis[moeda_origem] = [moeda_destino]
+    return dict_conversoes_disponiveis
+
+
